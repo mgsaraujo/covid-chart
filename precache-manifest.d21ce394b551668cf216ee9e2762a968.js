fa17803f7b4a79c4825585810ba19c9211f40346
cvt <cvt-2020@outlook.com>
2020-05-31 20:31:44 -0300

    Updates

---
+++ precache-manifest.d21ce394b551668cf216ee9e2762a968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d239f609f9c2bc7e6b591844affc1fb8",
+    "url": "/covid-chart/index.html"
+  },
+  {
+    "revision": "2b2b2aac2251a1120b33",
+    "url": "/covid-chart/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "89af0b0ffa8057e87951",
+    "url": "/covid-chart/static/js/2.829118a1.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid-chart/static/js/2.829118a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b2b2aac2251a1120b33",
+    "url": "/covid-chart/static/js/main.9d27a3b6.chunk.js"
+  },
+  {
+    "revision": "eb3ed7e439b056c2263c",
+    "url": "/covid-chart/static/js/runtime-main.01db2b1c.js"
+  }
+]);
